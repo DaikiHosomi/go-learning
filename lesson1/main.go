8d4ea130abff833b0a65ea078fb44c127b9e1db3
DaikiHosomi <daikho2021@gmail.com>
2021-07-12 23:26:31 +0900

    added main.go

---
+++ lesson1/main.go
@@ -0,0 +1,17 @@
+// packageは１つのみ
+package main
+
+
+import (
+	// format package -> print print line 主力ライン
+	"fmt"
+	// time package
+	"time"
+)
+
+//HelloWorld
+
+func main () {
+	fmt.Println("Hello World")
+	fmt.Println(time.Now())
+}
