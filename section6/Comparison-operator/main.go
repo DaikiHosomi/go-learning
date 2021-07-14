73fb9404180a21c14e8da4078241b1b208910872
DaikiHosomi <daikho2021@gmail.com>
2021-07-15 00:12:19 +0900

    added Comparison-operator

---
+++ section6/Comparison-operator/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+// 比較演算子
+
+func main() {
+	fmt.Println(1 == 1)
+	//true
+	fmt.Println(1 == 2)
+	// false
+	fmt.Println(4 <= 8)
+
+	fmt.Println(1 >= 8)
+	
+	fmt.Println(1 < 8)
+
+	fmt.Println(3 > 1)
+
+	fmt.Println(true == false)
+	// false
+
+	fmt.Println(true != false)
+	// true
+
+
+
+
+}
